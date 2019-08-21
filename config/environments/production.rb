86c7643df26e56f304a8d94e58430390e0c2d6c9
HtunLinn1 <htunlinn@mm.cybermissions.co.jp>
2019-08-21 10:45:34 +0630

    finished slack_app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'still-garden-12183.herokuapp.com'
+  host = 'warm-sea-49821.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
