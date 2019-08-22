9041e13564352a5e3c0994d0dc32a2bda8d91f5b
HtunLinn1 <htunlinn@mm.cybermissions.co.jp>
2019-08-22 10:09:28 +0630

    finish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'powerful-escarpment-81579.herokuapp.com'
+  host = 'desolate-wildwood-72548.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
