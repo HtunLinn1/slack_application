81d3f6a69ba5c0c5d2d14e6482ac2326d4dec84d
HtunLinn1 <htunlinn@mm.cybermissions.co.jp>
2019-08-26 09:25:19 +0630

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'thawing-garden-31552.herokuapp.com'
+  host = 'salty-harbor-24275.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
